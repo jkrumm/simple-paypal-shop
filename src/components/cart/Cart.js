700b62b12c508b574caae314ddd9086453e44e35
Tassilowilde <you@example.comreset>
2021-06-05 11:41:45 +0200

    Nochmal chart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -7,7 +7,7 @@ import {Link} from "react-router-dom";
 export default function Checkout() {
     return (
         <div>
-            <h1>Moin</h1>
+            <h1>Produkte</h1>
             <Button><Link to="/cart">Zur Kasse</Link></Button>
         </div>
     );
