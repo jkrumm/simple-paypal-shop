57808e439154c47403e64a2057c84f9cf06f1599
Tassilowilde <you@example.comreset>
2021-06-05 11:35:03 +0200

    Chart

--- src/containers/checkout/Checkout.js
+++ src/containers/checkout/Checkout.js
@@ -6,7 +6,7 @@ import PayPal from "../../components/paypal/PayPal";
 export default function Cart() {
     return (
         <div>
-            <h1>Moin</h1>
+            <h1>Hier werden Produkte aufgelistet die von Kunden in den Warenkorb gelegt wurden. Sie können gelöscht oder bezahlt werden.</h1>
             <PayPal/>
         </div>
     );
